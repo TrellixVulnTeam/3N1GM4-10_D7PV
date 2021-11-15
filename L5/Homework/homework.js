0a06ea7110dae0d35402b7c96299cc0e2419c78e
3N1GM4-10 <heculicharles25@gmail.com>
2021-11-15 16:53:38 +0800

    L5 Homework

--- L5/Homework/homework.js
+++ L5/Homework/homework.js
@@ -2,51 +2,48 @@
 
 //Example 1:
 function getBiggest(x, y) {
-  // x and y are integers.  Return the larger integer
-  // if they are the same return either one
+  return x >= y;
 }
 
 function greeting(language) {
-  // return a greeting for three different languages:
-  // language: 'German' -> 'Guten Tag!'
-  // language: 'Mandarin' -> 'Ni Hao!'
-  // language: 'Spanish' -> 'Hola!'
-  // if language is undefined return 'Hello!'
+  console.log('Guten Tag!, ' + German);
+  console.log('Ni Hao!, ' + Mandarin);
+  console.log('Hola!, ' + Spanish);
+  return Hello
 }
 
 function isTenOrFive(num) {
-  // return true if num is 10 or 5
-  // otherwise return false
+  returnIfNum(10 && 5);
+  return false;
 }
 
 function isInRange(num) {
-  // return true if num is less than 50 and greater than 20
-  // otherwise return false
+  rif (num < 50 && num > 20);
+  return false;
 }
 
 function isInteger(num) {
-  // return true if num is an integer
-  // 0.8 -> false
-  // 1 -> true
-  // -10 -> true
-  // otherwise return false
-  // hint: you can solve this using Math.floor
+  Math.floor(num)
+  (num > 0.8 ); // false
+  (num > 1); // true
+  (num > -10); //true
+  return false
 }
 
 function fizzBuzz(num) {
-  // if num is divisible by 3 return 'fizz'
-  // if num is divisible by 5 return 'buzz'
-  // if num is divisible by 3 & 5 return 'fizzbuzz'
-  // otherwise return num
+  if (num % 3); // return fizz
+  if (num % 5); // return buzz
+  if (num % 3 && num % 5); // return fizzbuzz
+  return num
 }
 
+
 function isPrime(num) {
-  // return true if num is prime.
-  // otherwise return false
-  // hint: a prime number is only evenly divisible by itself and 1
-  // hint2: you can solve this using a for loop
-  // note: 0 and 1 are NOT considered prime numbers
-}
+  for (let i =num    ; i <10            ; i++          ){
+  }
+    return false
+ }
+ 
 
 // Do not modify code below this line.
 // --------------------------------
