bdcda1cbb24391443d630d10ecdbdeaad9036d9d
3N1GM4-10 <heculicharles25@gmail.com>
2021-11-16 16:06:21 +0800

    L6 Homework

--- L6/Homework/homework.js
+++ L6/Homework/homework.js
@@ -1,65 +1,114 @@
 // Do not change any of the function names
 
 function returnFirst(arr) {
-  // return the first item from the array
+  const returnFirst = [10, 10, 16, 12];
+  returnFirst.shift(10);
+  const returnFirst = [97, 100, 80, 55, 72, 94];
+  returnFirst.shift(97);
 }
 
 function returnLast(arr) {
-  // return the last item of the array
+ const returnLast =[10, 10, 16, 12];
+ returnLast.pop(12);
+ const returnLast =[97, 100, 80, 55, 72, 94];
+ returnLast.pop(94);
+ const returnLast =['hi', 'there', 'how', 'are', 'you', 'doing?'];
+ returnLast.pop('doing');
 }
 
 function getArrayLength(arr) {
-  // return the length of the array
+  const getArrayLength = [10, 10, 16, 12]
+  console.log(getArrayLength.length);
+  const getArrayLength = [97, 100, 80, 55, 72, 94];
+  console.log(getArrayLength.length);
+  const getArrayLength = ['hi', 'there', 'how', 'are', 'you', 'doing?'];
+  console.log(getArrayLength.length);
 }
 
 function incrementByOne(arr) {
-  // arr is an array of integers  
-  // increase each integer by one
-  // return the array
+  for (var i = 0; i < arr; i++)
+    return (arr[i]);
 }
 
 function addItemToArray(arr, item) {
-  // add the item to the end of the array
-  // return the array
+  const addItemToArray = [10, 10, 16, 12];
+  addItemToArray.push(10);
+  const addItemToArray = [97, 100, 80, 55, 72, 94];
+  addItemToArray.push('Hello');
+  const addItemToArray =[]
+  addItemToArray.push(true);
 }
 
 function addItemToFront(arr, item) {
-  // add the item to the front of the array
-  // return the array
-  // hint: use the array method .unshift
+  const addItemToFront =[10, 10, 16, 12];
+  addItemToFront.unshift(10);
+  const addItemToFront =[97, 100, 80, 55, 72, 94];
+  addItemToFront.unshift('Hello');
+  const addItemToFront =[];
+  addItemToFront.unshift(true);
 }
 
 function wordsToSentence(words) {
-  // words is an array of strings
-  // return a string that is all of the words concatenated together
-  // spaces need to be between each word
-  // example: ['Hello', 'world!'] -> 'Hello world!'
+  function wordsToSentence(words) 
+    var sentence = ""; 
+    for (var i = 0; i < words.length; i++) {
+       sentence += 'AvionSchool' , 'Javascript' , 'Class' + words[i]; 
+    }
+    return sentence.trim(); 
 }
 
 function contains(arr, item) {
-  // check to see if item is inside of arr
-  // return true if it is, otherwise return false
+  array.includes()
+  return true
 }
 
 function addNumbers(numbers) {
-  // numbers is an array of integers.
-  // add all of the integers and return the value
+  const sum = [10, 10, 16].reduce(add, 0); 
+  function add(accumulator, a) 
+  return accumulator + a;
+}
+const sum = [97, 100].reduce(add, 0);{ 
+function add(accumulator, a) 
+return accumulator + a;
 }
 
 function averageTestScore(testScores) {
-  // testScores is an array.  Iterate over testScores and compute the average.
-  // return the average
+  var sum = 0;
+for( var i = 0; i < elmt.length; i++ ){
+    sum += parseInt( elmt[i], 10 );
+}
+
+var avg = sum/elmt.length;
 }
 
 function largestNumber(numbers) {
-  // numbers is an array of integers
-  // return the largest integer
-}
+  var array = [10, 10, 16, 12];
+  var largest= 16;
+for (i=0; i<=largest;i++)
+    if (array>largest) 
+        var largest=array[i];
+        
+        var array = [97, 100, 80, 55, 72, 94];
+        var largest= 100;
+         for (i=0; i<=largest;i++)
+            if (array>largest)
+                var largest=array[i];
+                
+                var array = [97, 10000, 80, 55, 7.2, -94];
+                var largest= 0;
+                
+                for (i=0; i<=largest;i++)
+                    if (array>largest) 
+                        var largest=array[i];         
+                    }
 
 function multiplyArguments() {
-  // use the arguments keyword to multiply all of the arguments together and return the product
-  // if no arguments are passed in return 0
-  // if one argument is passed in just return it
+  var multiply = function () {
+    var i, sum = 0;
+    for (i = 0; i < arguments.length; i++) 
+        sum *= arguments[i];
+    }
+    return sum;
 }
 // Do not modify code below this line.
 // --------------------------------
