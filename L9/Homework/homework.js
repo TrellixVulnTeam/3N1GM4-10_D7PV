7ea79d7f4f2cdf0003210c8d1b5be0a708329d8a
3N1GM4-10 <heculicharles25@gmail.com>
2021-11-19 15:42:34 +0800

    L9 Homework

--- L9/Homework/homework.js
+++ L9/Homework/homework.js
@@ -1,21 +1,41 @@
 // Do not change any of the function names
 
 function invokeCallback(cb) {
+  return cb = jest.fn;
   // invoke cb
 }
 
 function sumArray(numbers, cb) {
+  const nums = [1, 2, 3, 4, 5];
+     const sums = nums.reduce(function(acc, item){
+
+     });
   // sum up all of the integers in the numbers array
   // pass the result to cb
   // no return is necessary
 }
 
 function forEach(arr, cb) {
+  const nums = [1, 2, 3, 4, 5];
+     nums.forEach(function(item, index){
+      console.log(item);
+  });
   // iterate over arr and pass its values to cb one by one
   // hint: you will be invoking cb multiple times (once for each value in the array)
 }
 
 function map(arr, cb) {
+  const num = [1, 2, 3, 4, 5];
+    function squares (item) {
+      return num * item;
+    }
+   
+    const squares = nums.map(function(item) {
+      return item * num;
+    });
+
+    console.log(squares);
+
   // create a new array
   // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
   // the new array should be the same length as the array argument
