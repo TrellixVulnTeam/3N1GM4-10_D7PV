32a386af145a73884a977ba7f9fc3c1adce1b22c
3N1GM4-10 <heculicharles25@gmail.com>
2021-11-21 14:22:35 +0800

    L11 Homework

--- L11/Homework/homework.js
+++ L11/Homework/homework.js
@@ -3,15 +3,31 @@
 // solve these recursively
 
 function nFactorial(n) {
+ if (num < 0)
+    return -1;
+     
+    else if (num == 0)
+    return 1;
+
+      else {
+        return (num * factorialize(num - 1));
+      }
   // return the factorial for n
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
 }
 
 function nFibonacci(n) {
+  if(n == 0)
+        return 0;
+    else if(n == 1)
+      return 1;
+   else
+      return fibonacci(n - 1) + fibonacci(n - 2);
+}
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
-}
+
 
 // Do not modify code below this line.
 // --------------------------------
